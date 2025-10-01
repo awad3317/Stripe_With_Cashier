f51dd473bfaecdc77881e980ee031644cb1c107e
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-10-01 16:48:06 +0300

    update

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,8 +12,10 @@ class CheckoutController extends Controller
         $cart = Cart::where('session_id', request()->session()->getId())->first();
         $price = $cart->courses->pluck('stripe_price_id')->toArray();
         $sessionOptions = [
-            'success_url' => route('home', ['success' => true]),
-            'cancel_url' => route('home', ['success' => false]),
+            // 'success_url' => route('home', ['success' => true]),
+            'success_url' => route('checkout.success').'?session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url' => route('checkout.cancel').'?session_id={CHECKOUT_SESSION_ID}',
+            // 'cancel_url' => route('home', ['success' => false]),
            "metadata" =>[
             "cart_id" => $cart->id
            ]
@@ -26,5 +28,17 @@ class CheckoutController extends Controller
         // dd(Auth::user()->checkout($price, $sessionOptions));
         return Auth::user()->checkout($price, $sessionOptions, $customerOptions);
        
+    }
+
+    public function success(Request $request){
+        $session_id = $request->get('session_id');
+        $session= $request->user()->stripe()->checkout->sessions->retrieve($session_id);
+        dd($session);
+    }
+
+    public function cancel(Request $request){
+        $session_id = $request->get('session_id');
+        $session= $request->user()->stripe()->checkout->sessions->retrieve($session_id);
+        dd($session);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,8 @@ Route::controller(CartController::class)->group(function(){
 
 Route::controller(CheckoutController::class)->group(function(){
     Route::get('/checkout', 'checkout')->name('checkout')->middleware('auth');
-    Route::post('/checkout', 'process')->name('checkout.process');
+    Route::get('/checkout/success', 'success')->name('checkout.success')->middleware('auth');
+    Route::get('/checkout/cancel', 'cancel')->name('checkout.cancel')->middleware('auth');
 });
 
 Route::get('/dashboard', function () {
